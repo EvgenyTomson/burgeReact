bfed2181ecdd9fc5140b3364e77b15ef2e3bbedc
EvgenyTomson <tomilin.evgeny@gmail.com>
2023-02-25 18:36:39 +0300

    change API url

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,3 @@
-export const API_URI = 'http://localhost:3024';
+export const API_URI = 'https://kindhearted-sheer-message.glitch.me';
+//'http://localhost:3024';
 export const POSTFIX = 'api/product';
